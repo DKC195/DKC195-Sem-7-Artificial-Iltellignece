6e021cd11480b783c043ef9103e49ec4b3fa032e
DKC195 <Dhirajkc195@gmail.com>
2024-06-03 22:08:29 +0545

    Solved Problem 4

---
+++ Lab01/04_PrimeNumberGenerator.py
@@ -0,0 +1,14 @@
+def isPrime(x, tillx):
+  divided = 0
+  for i in tillx:
+    if (x % i) == 0:
+      divided = 1
+  
+  if divided == 0:
+    tillx.append(x)
+
+tillx = []
+for i in range(2, 100):
+  isPrime(i, tillx)
+
+print(tillx)
